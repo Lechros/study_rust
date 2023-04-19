9fda2fedcab082b47f990763eaec6875db546ee4
OniOniOn- <0onionion0@gmail.com>
2023-04-19 18:02:08 +0900

    feat(variables): variables and mutability

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,6 +1,24 @@
 fn main() {
-    let x = 5;
-    println!("The value of x is: {x}");
-    x = 6;
-    println!("The value of x is: {x}");
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+
+    {
+        let mut x = 5;
+        println!("The value of x is: {x}");
+        x = 6;
+        println!("The value of x is: {x}");
+    }
+    // OR
+    {
+        let x = 5; // 5
+        let x = x + 1; // 6
+        {
+            let x = x * 2; // 12
+            println!("The value of x in the inner scope is: {x}"); // 12
+        }
+        println!("The value of x is: {x}"); // 6
+    }
+    /*
+     * use shadowing to change variable's type,
+     * but mut cannot mutate type
+     */
 }
