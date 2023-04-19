5c6d4c91b01f7b06bee3f4f204c8e384e5570009
OniOniOn- <0onionion0@gmail.com>
2023-04-19 20:48:14 +0900

    feat(functions): statements and expressions

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,10 +1,23 @@
 fn main() {
-    println!("Hello, world!");
+    let y = {
+        let x = 3;
+        x + 1
+    }; // last expression of block is returned.
 
-    another_function();
+    println!("The value of y is: {y}");
+
+    print_labeled_measurement(5, 'h');
+
+    let x = five();
+
+    println!("five() is {x}");
 }
 
-// snake_case
-fn another_function() {
-    println!("Another function.");
+fn print_labeled_measurement(value: i32, unit_label: char) {
+    println!("The measurement is: {value}{unit_label}");
+}
+
+fn five() -> i32 {
+    let x = 5;
+    x // last expression should NOT end with semicolon, or it is statement.
 }
